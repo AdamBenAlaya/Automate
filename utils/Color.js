a7cd8c67fa05b7c919cb53af765213658a5e5c8a
KhalilFH <khalil.fekihhassen@medtech.tn>
2023-03-07 16:25:48 +0100

    AddCarScreen

---
+++ utils/Color.js
@@ -0,0 +1,11 @@
+const COLORS = {
+    white: '#fff',
+    black: '#000',
+    blue: '#5D5FEE',
+    grey: '#BABBC3',
+    light: '#F3F4FB',
+    darkBlue: '#7978B5',
+    red: 'red',
+    purple:'#23224B'
+  };
+  export default COLORS;
